ff971488aa55d15179a8400785c1237322e3f3b5
Dmitry Tochilin <ditochilin@gmail.com>
2019-01-13 00:53:53 +0200

    add project service, controller, validation

---
+++ PPMToolProject/src/main/java/com/myppm/ppmtool/web/ProjectController.java
@@ -0,0 +1,27 @@
+package com.myppm.ppmtool.web;
+
+import com.myppm.ppmtool.domain.Project;
+import com.myppm.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    protected ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+}
