6f39704900f32a14d1828054a7202dd0c3498c40
Dmitry Tochilin <ditochilin@gmail.com>
2019-01-16 15:05:15 +0200

    add store

--- PPMToolProject/src/main/java/com/myppm/ppmtool/domain/Project.java
+++ PPMToolProject/src/main/java/com/myppm/ppmtool/domain/Project.java
@@ -26,7 +26,7 @@ public class Project {
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
-    @JsonFormat(pattern = "yyyy-mm-dd")
+    @JsonFormat(pattern = "yyyy-mm-dd@HH:mm:ss")  // , locale = "ua_UA"
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
