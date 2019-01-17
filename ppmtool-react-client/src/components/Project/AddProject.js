ad42f496587662769fed5f311cf8184f2ee0119a
Dmitry Tochilin <ditochilin@gmail.com>
2019-01-17 23:36:02 +0200

    add project sending into the db

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -33,7 +33,7 @@ class AddProject extends Component {
       end_date: this.state.end_date
     };
 
-    // this.props.createProject(newProject, this.props.history);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -110,4 +110,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
