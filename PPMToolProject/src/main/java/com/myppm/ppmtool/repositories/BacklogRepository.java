de3e204e65da1c342681e131c39762ccb4bbe882
Dmitry Tochilin <ditochilin@gmail.com>
2019-01-20 20:50:28 +0200

    add backlog and projectTask

---
+++ PPMToolProject/src/main/java/com/myppm/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.myppm.ppmtool.repositories;
+
+import com.myppm.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String identifier);
+}
--- PPMToolProject/src/main/java/com/myppm/ppmtool/services/ProjectService.java
+++ PPMToolProject/src/main/java/com/myppm/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.myppm.ppmtool.services;
 
+import com.myppm.ppmtool.domain.Backlog;
 import com.myppm.ppmtool.domain.Project;
 import com.myppm.ppmtool.exceptions.ProjectException;
+import com.myppm.ppmtool.repositories.BacklogRepository;
 import com.myppm.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,25 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId()!=null){
+                project.setBacklog(
+                        backlogRepository.findByProjectIdentifier(
+                                project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
         } catch (Exception e){
             throw new ProjectException("Project ID: "+project.getProjectIdentifier()
